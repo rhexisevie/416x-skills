d0e3924fdef18930dee36f6d1146d51802f9e083
rhexisevie <alexandra.ma@scs.on.ca>
2026-01-06 19:51:21 -0500

    changing port nums fr

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,15 @@ using namespace pros;
 // radio is port 13
 // Chassis constructor
 ez::Drive chassis(
-    // These are your drive motors, the first motor is used for sensing!
-    {-1, 2, -3},   // Left Chassis Ports (negative port will reverse it!)
-    {17, -19, 20},  // Right Chassis Ports (negative port will reverse it!)
+    // drive motors, first motor used for sensing!
+    {-1, -2, -3},   // Left Chassis Ports (negative will reverse!)
+    {17, -19, 20},  // Right Chassis Ports (negative will reverse!)
 
     10,      // IMU Port
     3.25,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
     360);   // Wheel RPM = cartridge * (motor gear / wheel gear)
 
-// Uncomment the trackers you're using here!
+// Uncomment the trackers you're using
 // - `8` and `9` are smart ports (making these negative will reverse the sensor)
 //  - you should get positive values on the encoders going FORWARD and RIGHT
 // - `2.75` is the wheel diameter
